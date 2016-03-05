639e0decfe9fa833712b15bb94884006030f12c9
codeBelt <code@codebelt.com>
2016-03-05 09:16:19 -0600

    Initial setup and get package.json generating.

--- slushTasks/markupBuildSystem.js
+++ slushTasks/markupBuildSystem.js
@@ -5,19 +5,7 @@ module.exports = function (rootDirectory, answers) {
 
     gulp.task('markupBuildSystem', function(done) {
 
-        gulp.src(rootDirectory +'/templates/**')
-            //.pipe(template(answers))
-            //.pipe(rename(function (file) {
-            //    if (file.basename[0] === '_') {
-            //        file.basename = '.' + file.basename.slice(1);
-            //    }
-            //}))
-            //.pipe(conflict('./'))
-            .pipe(gulp.dest('./'))
-            //.pipe(install())
-            .on('end', function () {
-                done();
-            });
+
 
     });
 
--- slushTasks/requiredFiles.js
+++ slushTasks/requiredFiles.js
@@ -3,13 +3,14 @@ var gulp = require('gulp');
 module.exports = function (rootDirectory, answers) {
 
     gulp.task('requiredFiles', function(done) {
-        gulp.src(rootDirectory + '/none/**')
+        gulp.src(rootDirectory +'/templates/**')
             //.pipe(template(answers))
             //.pipe(rename(function (file) {
             //    if (file.basename[0] === '_') {
             //        file.basename = '.' + file.basename.slice(1);
             //    }
             //}))
+            //.pipe(conflict('./'))
             .pipe(gulp.dest('./'))
             //.pipe(install())
             .on('end', function () {
