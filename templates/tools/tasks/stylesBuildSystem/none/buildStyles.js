5569bfb09bbd5735b2adbafa92812cad0db73713
codeBelt <code@codebelt.com>
2016-03-09 20:23:27 -0600

    update comments

--- templates/tools/tasks/stylesBuildSystem/none/buildStyles.js
+++ templates/tools/tasks/stylesBuildSystem/none/buildStyles.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 
 gulp.task('buildStyles', function (done) {
     gulp
-        .src(env.DIR_SRC + '/**/*.css')
+        .src(env.DIR_SRC + '/assets/styles/**/*.css')
         .pipe(gulp.dest(env.DIR_DEST + '/assets/styles/'))
         .on('end', function () {
             done();
