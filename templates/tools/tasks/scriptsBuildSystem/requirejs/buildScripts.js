6b2a17e3f29bd0ec68e65875b4f6188c72dd37df
codeBelt <code@codebelt.com>
2016-03-13 20:41:31 -0500

    update comments

--- templates/tools/tasks/scriptsBuildSystem/requirejs/buildScripts.js
+++ templates/tools/tasks/scriptsBuildSystem/requirejs/buildScripts.js
@@ -1,22 +1,41 @@
 const gulp = require('gulp');
 const argv = require('yargs').argv;
-const requirejs = require('gulp-requirejs');
+var requirejsOptimize = require('gulp-requirejs-optimize');
+var sourcemaps = require('gulp-sourcemaps');
+//const requirejs = require('gulp-requirejs');
 
 gulp.task('buildScripts', (done) => {
+// https://github.com/requirejs/r.js/blob/master/build/example.build.js
+    gulp
+        .src(env.DIR_SRC + '/assets/scripts/main.js')
+        //.pipe(sourcemaps.init())
+        .pipe(requirejsOptimize((file) => {
+            return {
+                name: 'main',
+                baseUrl: env.DIR_SRC + '/assets/scripts/',
+                mainConfigFile : env.DIR_SRC + '/assets/scripts/config.js',
+                optimize: 'uglify2',
+                out: 'main.js',
+                useStrict: true
+            };
+        }))
+        //.pipe(sourcemaps.write())
+        .pipe(gulp.dest(env.DIR_DEST + '/assets/scripts/'))
+
     //if (isProd === true) {
-        requirejs({
-            name: 'main',
-            baseUrl: env.DIR_SRC + '/assets/scripts/',
-            out: 'main.js',
-            mainConfigFile : env.DIR_SRC + '/assets/scripts/config.js',
-            optimize: 'uglify2',
-            shim: {
-                // standard require.js shim options
-            }
-            // ... more require.js options
-        })
-            .pipe(gulp.dest(env.DIR_DEST + '/assets/scripts/'))
-            .on('end', done);
+    //    requirejs({
+    //        name: 'main',
+    //        baseUrl: env.DIR_SRC + '/assets/scripts/',
+    //        out: 'main.js',
+    //        mainConfigFile : env.DIR_SRC + '/assets/scripts/config.js',
+    //        optimize: 'uglify2',
+    //        shim: {
+    //            // standard require.js shim options
+    //        }
+    //        // ... more require.js options
+    //    })
+    //        .pipe(gulp.dest(env.DIR_DEST + '/assets/scripts/'))
+    //        .on('end', done);
     //} else {
     //    gulp
     //        .src(env.DIR_SRC + '/**/*.js')
@@ -29,13 +48,26 @@ gulp.task('buildScripts', (done) => {
 });
 
 
-//almond                 : true,
-//    name                   : "st",
-//    baseUrl                : "scripts",
-//    mainConfigFile         : "scripts/st.js",
-//    include                : "st",
-//    out                    : "scripts/st.build.js",
-//    wrap                   : true,
-//    optimize               : "uglify2",
-//    generateSourceMaps     : true,
-//    preserveLicenseComments: false
+
+//If using UglifyJS2 for script optimization, these config options can be
+//used to pass configuration values to UglifyJS2.
+//For possible `output` values see:
+//https://github.com/mishoo/UglifyJS2#beautifier-options
+//For possible `compress` values see:
+//https://github.com/mishoo/UglifyJS2#compressor-options
+//uglify2: {
+//    //Example of a specialized config. If you are fine
+//    //with the default options, no need to specify
+//    //any of these properties.
+//    output: {
+//        beautify: true
+//    },
+//    compress: {
+//        sequences: false,
+//            global_defs: {
+//            DEBUG: false
+//        }
+//    },
+//    warnings: true,
+//        mangle: false
+//},
