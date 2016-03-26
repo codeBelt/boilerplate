d13587a08089f46a56f2dc5cdf837f4b4c8d0e00
codeBelt <code@codebelt.com>
2016-03-26 13:03:31 -0500

    Initial

--- slush/tasks/scriptsBuildSystem.js
+++ slush/tasks/scriptsBuildSystem.js
@@ -6,6 +6,8 @@ const template = require('gulp-template');
 
 module.exports = (rootDirectory, answers) => {
 
+    const wasDemoChoosen = answers.demoBuildSystem === 'no';
+
     const type = answers.scriptsBuildSystem;
     let devDependencies = [];
     let bowerDependencies = [];
@@ -27,13 +29,28 @@ module.exports = (rootDirectory, answers) => {
     }
 
     const taskPath = rootDirectory + '/templates/tools/tasks/scriptsBuildSystem/' + type + '/buildScripts.js';
+    const sourcePath = rootDirectory + '/templates/src/scriptsBuildSystem/' + type + '/**/*';
 
     // Gulp task
     gulp.task('scriptsBuildSystem', (done) => {
-        gulp
+        const tasks = [];
+
+        const copyTasks =gulp
             .src(taskPath)
-            .pipe(gulp.dest('./tools/tasks/'))
-            .on('end', done);
+            .pipe(gulp.dest('./tools/tasks/'));
+
+        tasks.push(copyTasks);
+
+        if (wasDemoChoosen === false) {
+            const copySourceFiles = gulp
+                .src(sourcePath)
+                .pipe(template(answers))
+                .pipe(gulp.dest('./src/'));
+
+            tasks.push(copySourceFiles);
+        }
+
+        return merge(...tasks);
     });
 
     // Return data
