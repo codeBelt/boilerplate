9e15e549173435a4d3e8f52763601eaadbeba856
codeBelt <code@codebelt.com>
2016-05-07 21:31:37 -0500

    Add blurb.

--- slush/tasks/demoBuildSystem/files/scripts/requirejs/src/assets/scripts/main.js
+++ slush/tasks/demoBuildSystem/files/scripts/requirejs/src/assets/scripts/main.js
@@ -1,10 +1,25 @@
-// The only purpose of this file is to kick off your application's top-level
-// controller at the appropriate time. All other code should be written as
-// separate modules in their own files.
-//
-// Note that since this is the application entry-point, traditional
-// RequireJS syntax is used here to specify dependencies. Do not use this
-// syntax in any other modules.
+/*
+
+ ╔══════════════════════════════════════════════════════════════════════════════════════╗
+ ║ Naming Convention                                                                    ║
+ ╠══════════════════════════════════════════════════════════════════════════════════════╣
+ ║ Anytime JavaScript interact with an element. Prepend the selector name with a 'js-'. ║
+ ║ - Example: js-someButton                                                             ║
+ ║                                                                                      ║
+ ║ Name the selector the same name as the view.                                         ║
+ ║ - Example: SomeView would have a selector named js-SomeView                          ║
+ ╚══════════════════════════════════════════════════════════════════════════════════════╝
+
+ The only purpose of this file is to kick off your application's top-level
+ controller at the appropriate time. All other code should be written as
+ separate modules in their own files.
+
+ Note that since this is the application entry-point, traditional
+ RequireJS syntax is used here to specify dependencies. Do not use this
+ syntax in any other modules.
+
+ http://plaintexttools.github.io/plain-text-table/
+ */
 
 /**
  * Bootstrap.
